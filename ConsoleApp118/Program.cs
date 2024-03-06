b21531041af61d4ca0f894576768828125e6d8ec
ygursu <yasemingrs@gmail.com>
2024-03-06 23:34:02 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp118/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Sayı giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int toplam = sayi;
+int i = 0, sayac = 1;
+
+while (sayi != 0)
+{
+    Console.WriteLine("Sayı giriniz:");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    toplam += sayi;
+    if (sayi != 0)
+        sayac++;
+}
+
+Console.WriteLine("Girilen sayıların toplamı={0} Ortalaması={1}", toplam, toplam / sayac);
+Console.ReadKey();
